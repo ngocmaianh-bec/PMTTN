f1885b9eefd715e7888935cce65d828cd5072e61
Binh Thanh <dtb1232005@gmail.com>
2025-10-29 14:29:17 +0700

    frmtest vs test result ok

--- FrmTestResult.Designer.cs
+++ FrmTestResult.Designer.cs
@@ -28,12 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpBoxTTHV = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.grpBoxTTHV.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBoxTTHV
+            // 
+            this.grpBoxTTHV.BackColor = System.Drawing.SystemColors.Control;
+            this.grpBoxTTHV.Controls.Add(this.textBox4);
+            this.grpBoxTTHV.Controls.Add(this.label4);
+            this.grpBoxTTHV.Controls.Add(this.textBox3);
+            this.grpBoxTTHV.Controls.Add(this.label3);
+            this.grpBoxTTHV.Controls.Add(this.textBox1);
+            this.grpBoxTTHV.Controls.Add(this.label1);
+            this.grpBoxTTHV.Location = new System.Drawing.Point(12, 12);
+            this.grpBoxTTHV.Name = "grpBoxTTHV";
+            this.grpBoxTTHV.Size = new System.Drawing.Size(807, 106);
+            this.grpBoxTTHV.TabIndex = 17;
+            this.grpBoxTTHV.TabStop = false;
+            this.grpBoxTTHV.Text = "Thông tin học viên & môn thi";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(632, 56);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 26);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(565, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Môn thi";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(386, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(141, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(322, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mã HV";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ và tên";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(702, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 43);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.ForeColor = System.Drawing.Color.Brown;
+            this.textBox8.Location = new System.Drawing.Point(279, 299);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(320, 43);
+            this.textBox8.TabIndex = 25;
+            this.textBox8.Text = "100/100";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(198, 299);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 26);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Điểm:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(314, 262);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(285, 26);
+            this.textBox7.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(194, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 26);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Đánh giá:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(419, 217);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(180, 26);
+            this.textBox6.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(194, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(219, 26);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Số câu trả lời đúng:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(332, 175);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(267, 26);
+            this.textBox5.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(194, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 26);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Số câu hỏi:";
+            // 
+            // FrmTestResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(834, 450);
+            this.Controls.Add(this.grpBoxTTHV);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Name = "FrmTestResult";
             this.Text = "FrmTestResult";
+            this.grpBoxTTHV.ResumeLayout(false);
+            this.grpBoxTTHV.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpBoxTTHV;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
     }
 }
