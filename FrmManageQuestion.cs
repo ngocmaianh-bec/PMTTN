94d8eabc1476c8e5e5d02527673daddb44e7fff1
pthaogw <ngphthaogw@gmail.com>
2025-10-29 14:20:11 +0700

    frm manage question ok

---
+++ FrmManageQuestion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phần_mềm_thi_trắc_nghiệm
+{
+    public partial class FrmManageQuestion : Form
+    {
+        public FrmManageQuestion()
+        {
+            InitializeComponent();
+        }
+
+        private void grbTTCH_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
